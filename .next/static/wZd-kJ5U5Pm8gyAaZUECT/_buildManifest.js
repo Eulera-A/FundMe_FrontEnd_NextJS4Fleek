255b15117bd611e8710e84341d39a26cdda25295
Eulera <feiliujustina@hotmail.com>
2025-05-25 14:39:39 -0700

    ready for re deploy with secured contract

--- .next/static/wZd-kJ5U5Pm8gyAaZUECT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-6bc70d4c7f2284f7.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
